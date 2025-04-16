71ac0a77b8e055133408b0026636393f01e13f60
gauravghimiree <ghimiregaurav773@gmail.com>
2025-04-16 22:24:57 +0545

    modelmapper has been added here

--- src/main/java/com/example/demo/Controller/ProductController.java
+++ src/main/java/com/example/demo/Controller/ProductController.java
@@ -1,26 +1,42 @@
 package com.example.demo.Controller;
 
 
+import com.example.demo.DTO.ProductDTO;
 import com.example.demo.Model.Product;
 import com.example.demo.Service.ProductService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
+
+   @Autowired
+   ModelMapper modelMapper;
 
     @Autowired
     private ProductService productService;
 
     @PostMapping("/add")
-    public String saveProduct(@RequestBody Product product) {
-        productService.save(product);
+    public String saveProduct(@RequestBody ProductDTO product) {
+       Product p= modelMapper.map(product, Product.class);
+       productService.save(p);
         return "success";
     }
+    @GetMapping("/lists")
+    public List<ProductDTO> getProducts() {
+      List < Product> products = productService.getAllProducts();
+      return products
+              .stream()
+              .map(p->modelMapper.map(p,ProductDTO.class))
+              .collect(Collectors.toList());
 
+
+    }
 
     }
 
